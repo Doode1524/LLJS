942a3e36dea9f08e5773fb31a9f79154912bc6d2
joey <doode1524@gmail.com>
2021-07-03 15:53:58 -0400

    bst v2

---
+++ bst2.js
@@ -0,0 +1,52 @@
+
+
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+class BST {
+  constructor() {
+    this.root = null;
+  }
+  insert(value) {
+    const newNode = new Node(value);
+
+    if (!this.root) {
+      this.root = newNode;
+      return this;
+    }
+    let current = this.root;
+    while (true) {
+      if (value === current.value) return undefined;
+      if (value < current.value) {
+        if (!current.left) {
+          current.left = newNode;
+          return this;
+        }
+        current = current.left;
+      } else if (value > current.value) {
+        if (!current.right) {
+          current.right = newNode;
+          return this;
+        }
+        current = current.right;
+      }
+    }
+  }
+}
+
+const tree = new BST();
+
+tree.insert(20);
+tree.insert(10);
+tree.insert(5);
+tree.insert(15);
+tree.insert(30);
+tree.insert(25);
+tree.insert(32);
+
+console.log(treeify.asTree(tree, true));
