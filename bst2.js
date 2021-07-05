5ba2b80f580975aa0f0f60c29426ee751fa3d135
joey <doode1524@gmail.com>
2021-07-05 16:09:01 -0400

    bfs and delete

--- bst2.js
+++ bst2.js
@@ -8,7 +8,6 @@ class Node {
   }
 }
 
-
 class BST {
   constructor() {
     this.root = null;
@@ -55,6 +54,56 @@ class BST {
     if (!found) return "Nothing Found!";
     return current;
   }
+  BFS(start) {
+    let data = [],
+      queue = [],
+      current = start ? this.find(start) : this.root;
+
+    queue.push(current);
+    while (queue.length) {
+      current = queue.shift();
+      data.push(current.val);
+
+      if (current.left) queue.push(current.left);
+      if (current.right) queue.push(current.right);
+    }
+
+    return data;
+  }
+  delete(val) {
+    if (!this.root) return undefined;
+    let current = this.root,
+      parent;
+
+    const pickSide = (side) => {
+      if (!current[side]) return "No node found!";
+
+      parent = current;
+      current = current[side];
+    };
+
+    const deleteNode = (side) => {
+      if (current.val === val && current.count > 1) current.count--;
+      else if (current.val === val) {
+        const children = this.BFS(current.val);
+        parent[side] = null;
+        children.splice(0, 1);
+        children.forEach((child) => this.create(child));
+      }
+    };
+
+    while (current.val !== val) {
+      if (val < current.val) {
+        pickSide("left");
+        deleteNode("left");
+      } else {
+        pickSide("right");
+        deleteNode("right");
+      }
+    }
+
+    return current;
+  }
 }
 
 const tree = new BST();
@@ -69,9 +118,9 @@ tree.insert(32);
 tree.insert(12);
 tree.insert(16);
 
-console.log(tree)
-console.log(tree.find(20), 'find')
-
+console.log(tree.BFS(20))
+console.log(tree);
+// console.log(tree.find(20), "find");
 // console.log(treeify.asTree(tree, true));
 
 // class BST {
